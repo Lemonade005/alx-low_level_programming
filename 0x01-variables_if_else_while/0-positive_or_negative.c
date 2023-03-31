5aad0bf953e1cf9bc79c4baa58eb618e71cb4b11
lemonade005 <piusmwangi05@gmail.com>
2023-03-31 11:00:12 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,29 +1,28 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 /**
- *This program will assign a random number to the variable n
- *
- */
+* main - Prints if number is positive, zero or negative
+*
+* Return: Always (Success)
+*/
 int main(void)
-	/* This program will assign a random number to the variable n*/
-		{
-	int n;
+{
+int n;
 srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
 
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,26 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-#include<math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints the last digit of the random
+ * number stored in the variable n
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-       int x;
-     srand(time(0));
-int n = rand() /* - RAND_MAX \ 2; */
-x = n % 10;
-/* printf ("last digit of" , n , "is" , x);*/
-if (x > 5)
-{
-printf (" and is greater than 5");
-}
-else if (x == 0)
-{
-printf ("and is equal to 0");
-{
-else if (x < 6)
-{
-printf ("and is less than 6 and not 0");
-}
-return (0);
-}
+	int n;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	printf("Last digit of %d is %d ", n, n % 10);
+	if (n % 10 > 5)
+		printf("and is greater than 5\n");
+	else if (n % 10 == 0)
+		printf("and is 0\n");
+	else if (n % 10 < 6 && n % 10 != 0)
+		printf("and is less than 6 and not 0\n");
+	return (0);
+}
